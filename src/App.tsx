e12651face3f0bf48304ec213d3c17439eea881e
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-18 16:37:29 +0100

    ✨ Call API from client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function doFetch() {
+      const response = await fetch("/api/hello");
+      const result = await response.json();
+      setMessage(result.message);
+    }
+    doFetch();
+
+    // fetch('/api/hello')
+    //   .then((response) => response.json())
+    //   .then((result) => setMessage(result.message));
+  }, []);
+
   return (
     <div className={styles.app}>
-      <header></header>
+      <p>Hello Vite + React!</p>
+      <p>{message}</p>
     </div>
   );
 }
-
 export default App;
